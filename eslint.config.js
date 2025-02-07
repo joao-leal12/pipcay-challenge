c4b60fddbce7652a772b4f0f861d6953006a47f8
João Victor <97269781+joao-leal12@users.noreply.github.com>
2025-02-07 00:50:35 -0300

    retirar regras de lint

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,9 @@ export default [
     rules:{ 
 
       'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'off'
+      '@typescript-eslint/no-unused-vars': 'off',
+       '@typescript-eslint/no-explicit-any': 'off',
+       '@typescript-eslint/no-empty-object-type': 'off'
     }
   }
  
